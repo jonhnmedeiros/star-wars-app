09afc524b5a6c826c9cd488ad23340b504414f20
Jonathan Felipe Medeiros <jonathan.info@gmail.com>
2021-03-31 09:56:08 -0300

    commit

---
+++ src/api/generic-api.ts
@@ -0,0 +1,26 @@
+import { getAxiosInstance } from "./axios-instance";
+
+export type SWAPIEndpoint = 'people' | 'films' | 'starships' | 'vehicles' | 'species' | 'planets';
+
+export interface ResourceReturn {
+  getSchema:()=>void;
+  getById:(id:number)=>Promise<any>;
+}
+
+export const genericController = (endpoint:SWAPIEndpoint):ResourceReturn => {
+  const axios = getAxiosInstance();
+
+  const getSchema = async () => {
+    const response = await axios.get(`/${endpoint}/schema`);
+    return response.data;
+     
+  }
+
+  const getById = async (id:number) =>{
+    const response = await axios.get(`/${endpoint}/${id}`)
+    return response.data;
+  }
+
+  return {getSchema, getById}
+
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,14 @@
+import React, {useEffect, useState} from 'react'
+import { genericController } from './api/generic-api';
 
-export const App = () =>{
+export const App = ()=>{
+  const [schema, setSchema] = useState({name:''});
+  const {getById} = genericController('people');
+  useEffect(()=>{
+    getById(1).then(valor=>setSchema(valor));
+  },[])
+  
   return (
-    <div>Hello Luke!!!!!</div>
+    <div> {schema.name} </div>
   )
 }
